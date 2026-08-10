63de8821c58a8586ae02875ce6324e2cac74641c
Aldane Hutchinson <adminalchicken@gmail.com>
2026-08-09 21:09:29 -0500

    deletes uncessary files

--- src/directive.js
+++
@@ -1,6 +0,0 @@
-import { pluginAPI } from './plugin';
-export function defineDirective(name, fn) {
-    pluginAPI.registerDirective(name, fn);
-}
-// Re-export getDirective from plugin
-export { getDirective } from './plugin';
